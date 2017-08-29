82ab388b666fd06b60cf22d8975c8ade543c4920
gered <gered@blarg.ca>
2017-08-29 14:59:06 -0400

    version 0.1-alpha2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-htmltopdf "0.1-alpha"
+(defproject clj-htmltopdf "0.1-alpha2"
   :description  "Simple Clojure wrapper for Open HTML to PDF"
   :url          "https://github.com/gered/clj-htmltopdf"
   :license      {:name "GNU Lesser General Public License v3.0"
