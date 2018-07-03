01f12c6f2c6bc56ea34b3c3e11563c3aa7b9eae6
gered <gered@blarg.ca>
2018-07-03 11:12:19 -0400

    bump clojure version ... long overdue. oops

--- project.clj
+++ project.clj
@@ -13,7 +13,7 @@
                  [hiccup "1.0.5"]]
 
   :profiles     {:provided
-                 {:dependencies [[org.clojure/clojure "1.8.0"]]}
+                 {:dependencies [[org.clojure/clojure "1.9.0"]]}
 
                  :dev
                  {:dependencies [[pjstadig/humane-test-output "0.8.1"]]
